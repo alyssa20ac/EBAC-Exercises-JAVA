0814deb045107afb3b7fac7ec15eab2430340a01
edgoad <alyssa.olv20@gmail.com>
2024-01-23 22:54:14 +0000

    homework

---
+++ CalculoMedia/src/CalculoMedia.java
@@ -0,0 +1,15 @@
+public class CalculoMedia {
+    public static void main(String[] args) {
+        System.out.println("***Calculo de Media***");
+
+        int nota1 = 9;
+        int nota2 = 7;
+        int nota3 = 8;
+        int nota4 = 9;
+
+        // Calcula a média corretamente
+        double media = (nota1 + nota2 + nota3 + nota4) / 4.0;
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
